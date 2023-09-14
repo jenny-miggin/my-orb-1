a4fcbc1e49a7312774c191c19ebf7ac82f357853
Jenny Miggin <jenny@circleci.com>
2023-09-14 14:13:57 +0100

    v6 rerelease orb1

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo Hello again from orb1 "${PARAM_TO}" updating v4 instead of publishing a new release
+echo Hello again from orb1 "${PARAM_TO}" updating v6 instead of publishing a new release
